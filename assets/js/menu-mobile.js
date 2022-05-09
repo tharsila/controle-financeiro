08be1b53f32d3541dd1abcaf50239ada295fe9a7
tharsila <tharsilapaula@gmail.com>
2022-05-09 06:18:15 -0300

    add menu mobile

---
+++ assets/js/menu-mobile.js
@@ -0,0 +1,25 @@
+export default function menuMobile () {
+  const buttonMobile = document.getElementById('menu-mobile');
+  const menu = document.querySelector('.menu');
+  const links = document.querySelectorAll('.menu a');
+  
+  /* Evento que será realizado quando o icone mobile for ativado */
+  buttonMobile.addEventListener('click', handleMenu);
+
+  /* Torna itens do menu animados */
+  function animationLinks () {
+    links.forEach((link, index) => {
+      link.style.animation 
+      ? (link.style.animation = '') 
+      : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`)
+    })
+  }
+  
+  /* Exibe menu mobile e seus itens */
+  function handleMenu () {
+    buttonMobile.classList.toggle('fa-times');
+    menu.classList.toggle('active');
+    animationLinks();
+  }
+}
+

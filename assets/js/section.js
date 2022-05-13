3ecde4657d6a35402716b104290aac688e983da7
tharsila <tharsilapaula@gmail.com>
2022-05-13 03:32:19 -0300

    add localStorage

--- assets/js/section.js
+++ assets/js/section.js
@@ -4,14 +4,18 @@ export default function section () {
 
 /* recebe os produtos adicionados atraves do push */
 let elements = [];
-/* const elementsRaw = localStorage.getItem('elements');
+
+/* recebe os dados que o usuário cadastrou */
+const elementsRaw = localStorage.getItem('elements');
   if (elementsRaw !== null) {
     elements = JSON.parse(elementsRaw);
   } else {
     elements = [];
-  } */
+  }
 
-/* Desenha a tabela */
+  drawTable();
+
+/* desenha a tabela com os produtos atualizados */
 function drawTable () {
   const currentLines = [...document.querySelectorAll('.dynamic-content')];
   const result = document.getElementById('result');
@@ -25,14 +29,13 @@ function drawTable () {
   elements.forEach((element) => { 
     result.innerHTML += `
       <tr class="flex dynamic-content">
-        <td class="transaction">${element.choose === '-' ? "-" : "+"}</td>
+        <td class="transaction">${element.choose === '-' ? '-' : '+'}</td>
         <td class="product ">${element.name}</td>
         <td class="price width">R$ ${element.price}</td> 
       </tr>
     `
     /* realiza calculo para saber se existe prejuízo ou lucro */
     let price = parseFloat(element.price)
-    console.log(price)
     if (element.choose === '-') {
       total -= price
     } else if (element.choose === '+') {
@@ -45,6 +48,7 @@ function drawTable () {
 
   /* adicionar linha da tabela mostrando o valor total dos produtos */
   const foot = document.getElementById('total');
+  
   foot.innerHTML = `
     <tr class="flex">
       <td class="left">Total</td>
@@ -53,13 +57,45 @@ function drawTable () {
       </td>
       <td></td>
     </tr>
-  <tr class="flex">
+   <tr class="flex">
+     <td></td>
+     <td></td>
+     <td class="profit width">${total > 0 ? '[LUCRO]' : total < 0 ? '[PREJUÍZO]' : ""}</td> 
     <td></td>
-    <td></td>
-    <td class="profit width">${total > 0 ? '[LUCRO]' : '[PREJUÍZO]'}</td>
-    <td></td>
-  </tr>
+   </tr>
   `
+  /* verifica se existe produtos cadastrados */
+  if (localStorage.length === 0) {
+    result.innerHTML = `
+     <tr class="flex dynamic-content">
+        <td class="transaction"></td>
+        <td class="product" style="text-align:center; width: 100%">Nenhuma Transação cadastrada</td>
+      </tr>
+      <tr class="flex">
+        <td class="line-one width"></td>
+        <td class="line-two width" colspan="2"></td>
+      </tr>
+    `
+  }
+}
+
+/* limpa o conteudo da tabela */
+function cleanTable () {
+  document.querySelector('tbody').innerHTML = "";
+  document.querySelector('tfoot').innerHTML = "";
+}
+
+const clean = document.querySelector('.clean');
+clean.addEventListener("click", cleanData);
+
+/* limpa o conteudo da tabela ativado o evento de click */
+function cleanData () {
+  if (confirm('Você tem certeza que deseja limpar os dados da tabela?')) {
+    localStorage.clear();
+    elements = [];
+    cleanTable();
+    drawTable();
+  }
 }
 
 /* captura form no html */
@@ -77,6 +113,18 @@ function handleSubmit(event) {
       "price": +event.target.elements["price"].value,
     },
   )
-  /* desenha a tabela com os produtos atualizados */
-  drawTable()
+
+  /* apaga dados preenchidos quando o formulario for submetido */
+  document.getElementById('choose').value = '-';
+  document.getElementById('name').value = '';
+  document.getElementById('price').value = '';
+
+ 
+    /* persiste os dados que o usuário cadastrou */
+  localStorage.setItem('elements', JSON.stringify(elements));
+  if (elements !== null) {
+    cleanTable()
+    drawTable()
+  }
+
 }

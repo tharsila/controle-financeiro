902f5360b1c9aa98b949a11c93bd5da80d1ae574
tharsila <tharsilapaula@gmail.com>
2022-05-15 03:31:57 -0300

    corrige bugs

--- assets/js/section.js
+++ assets/js/section.js
@@ -1,89 +1,61 @@
-export default function section () {
+export default function section() {
   /* recebe os produtos adicionados atraves do push */
   let elements = [];
-  
-  /* recebe os dados que o usuário cadastrou */
+  /* captura os elementos do html */
+  const inputPrice = document.getElementById('price');
+  const clean = document.querySelector('.clean');
+  const form = document.getElementById('form');
+
+  /* recebe os dados que o usuário cadastroue armazena no localStorage */
   const elementsRaw = localStorage.getItem('elements');
-    if (elementsRaw !== null) {
-      elements = JSON.parse(elementsRaw);
-    } else {
-      elements = [];
-    }
-    
-  drawTable();
-  
-  /* Coloca o valor do produto digitado no formato BRL */
-  const regexp = /[^0-9]/;
-  let inputPrice = document.getElementById('price');
-  inputPrice.addEventListener('keypress', maskedValue);
-  
-  function maskedValue(event) {
-    if (regexp.test(event.key)) {
-      event.preventDefault();
-      return
-    }
-  
-    if(!event.target.value) return;
-    let priceFormat = 0;
-    priceFormat = event.target.value.toString();
-    priceFormat = priceFormat.replace(/[\D]+/g, '');
-    priceFormat = priceFormat.replace(/([0-9]{1})$/g, ",$1");
-  
-    if(priceFormat.length >= 6){
-      while(/([0-9]{4})[,|\.]/g.test(priceFormat)){
-        priceFormat = priceFormat.replace(/([0-9]{1})$/g, ",$1");
-        priceFormat = priceFormat.replace(/([0-9]{3})[,|\.]/g, ".$1");
-      }
-    }
-  
-    event.target.value = priceFormat;
+  if (elementsRaw !== null) {
+    elements = JSON.parse(elementsRaw);
+  } else {
+    elements = [];
   }
-  
+
+  /* formata o valor para Real "R$" */
+  function formatterCurrency(value) {
+    const valueFormat = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+    return valueFormat;
+  }
+
   /* desenha a tabela com os produtos atualizados */
-  function drawTable () {
+  function drawTable() {
     const currentLines = [...document.querySelectorAll('.dynamic-content')];
     const result = document.getElementById('result');
-  
-    currentLines.forEach((element) => {
-      element.remove();
-    })
-  
-    /* percorre pelos elementos cadastrados e mostra na tela */
+
+    currentLines.forEach((element) => element.remove());
+
     let total = 0;
-
-
-    //formata o valor para Real "R$"
-    function formatterCurrency(value) {
-      const valueFormat = value.toLocaleString("pt-BR", {
-        style: "currency",
-        currency: "BRL",
-      });
-      return valueFormat;
-    }
-
-    elements.forEach((element) => { 
+    /* percorre pelos elementos cadastrados e mostra na tela */
+    elements.forEach((element) => {
       result.innerHTML += `
         <tr class="flex dynamic-content">
           <td class="transaction">${element.choose === '-' ? '-' : '+'}</td>
           <td class="product ">${element.name}</td>
           <td class="price width">R$ ${formatterCurrency(element.price)}</td> 
         </tr>
-      `
+      `;
 
       /* realiza calculo para saber se existe prejuízo ou lucro */
-      let price = element.price;
-      
+      const { price } = element;
       if (element.choose === '-') {
-        total -= price
-        console.log(total)
+        total -= Number(price);
       } else if (element.choose === '+') {
-        total += price
-        console.log(total)
+        total += Number(price);
       }
-    })
-    
+    });
+
     /* formata o valor do produto para o real(moeda brasileira) */
-    const priceBRL = total.toLocaleString('pt-BR',{style: 'currency', currency: 'BRL', maximumFractionDigits: 2});
+    const priceBRL = total.toLocaleString('pt-br', {
+      style: 'currency',
+      currency: 'BRL',
+      maximumFractionDigits: 2,
+    });
 
     /* adiciona linha da tabela mostrando o valor total dos produtos */
     const foot = document.getElementById('total');
@@ -98,10 +70,11 @@ export default function section () {
      <tr class="flex">
        <td></td>
        <td></td>
-       <td class="profit width">${total > 0 ? '[LUCRO]' : total < 0 ? '[PREJUÍZO]' : ""}</td> 
+       <td class="profit width">${total > 0 ? '[LUCRO]' : total < 0 ? '[PREJUÍZO]' : ''}</td> 
       <td></td>
      </tr>
-    `
+    `;
+
     /* verifica se existe produtos cadastrados */
     if (localStorage.length === 0) {
       result.innerHTML = `
@@ -113,21 +86,44 @@ export default function section () {
           <td class="line-one width"></td>
           <td class="line-two width" colspan="2"></td>
         </tr>
-      `
+      `;
     }
   }
-  
-  /* limpa o conteudo da tabela */
-  function cleanTable () {
-    document.querySelector('tbody').innerHTML = "";
-    document.querySelector('tfoot').innerHTML = "";
+
+  drawTable();
+
+  /* Coloca o valor do produto digitado no formato BRL */
+  function maskedValue(event) {
+    const regexp = /[^0-9]/;
+    if (regexp.test(event.key)) {
+      event.preventDefault();
+      return;
+    }
+
+    if (!event.target.value) return;
+    let priceFormat = 0;
+    priceFormat = event.target.value.toString();
+    priceFormat = priceFormat.replace(/[\D]+/g, '');
+    priceFormat = priceFormat.replace(/([0-9]{1})$/g, ',$1');
+
+    if (priceFormat.length >= 6) {
+      while (/([0-9]{4})[,|.]/g.test(priceFormat)) {
+        priceFormat = priceFormat.replace(/([0-9]{1})$/g, ',$1');
+        priceFormat = priceFormat.replace(/([0-9]{3})[,|.]/g, '.$1');
+      }
+    }
+    event.target.value = priceFormat;
   }
-  
-  const clean = document.querySelector('.clean');
-  clean.addEventListener("click", cleanData);
-  
+  inputPrice.addEventListener('keypress', maskedValue);
+
+  /* limpa o conteudo da tabela */
+  function cleanTable() {
+    document.querySelector('tbody').innerHTML = '';
+    document.querySelector('tfoot').innerHTML = '';
+  }
+
   /* limpa o conteudo da tabela ativado o evento de click */
-  function cleanData () {
+  function cleanData() {
     if (confirm('Você tem certeza que deseja limpar os dados da tabela?')) {
       localStorage.clear();
       elements = [];
@@ -135,35 +131,31 @@ export default function section () {
       drawTable();
     }
   }
-  
-  /* captura form no html */
-  const form = document.getElementById('form');
-  /* aciona o evento de submit e chama a função */
-  form.addEventListener('submit', handleSubmit);
-  
-  /* ativa a função submit e 'empurra' o objeto para o array elements*/
+
+  /* ativa a função submit e 'empurra' o objeto para o array elements */
   function handleSubmit(event) {
     event.preventDefault();
     elements.push(
       {
-        choose: event.target.elements['choose'].value,
-        name: event.target.elements['name'].value,
-        price: event.target.elements['price'].value.replaceAll('.', '').replaceAll(',', '.')
+        choose: event.target.elements.choose.value,
+        name: event.target.elements.name.value,
+        price: event.target.elements.price.value.replaceAll('.', '').replaceAll(',', '.'),
       },
-    )
-  
+    );
+
     /* apaga dados preenchidos quando o formulario for submetido */
     document.getElementById('choose').value = '-';
     document.getElementById('name').value = '';
     document.getElementById('price').value = '';
-  
-   
     /* persiste os dados que o usuário cadastrou */
+
     localStorage.setItem('elements', JSON.stringify(elements));
     if (elements !== null) {
-      cleanTable()
-      drawTable()
+      cleanTable();
+      drawTable();
     }
   }
+  /* aciona o evento de submit e chama a função */
+  form.addEventListener('submit', handleSubmit);
+  clean.addEventListener('click', cleanData);
 }
-
